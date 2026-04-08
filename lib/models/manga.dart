792dec6764a5a180f6aa90321df6f991362d92aa
fa-fifi <fafifi1997@gmail.com>
2026-04-09 03:47:35 +0800

    💄 Render two-panel genre browser layout

--- lib/models/manga.dart
+++ lib/models/manga.dart
@@ -1,14 +1,20 @@
+import 'package:mangatrack/models/genre.dart';
+
 class Manga {
   final int id;
   final String title;
-  final Map<String, dynamic> images;
-
-  const Manga({required this.id, required this.title, required this.images});
+  final Map<String, dynamic> _images;
+  final List<dynamic> _genres;
 
   Manga.fromJson(Map<String, dynamic> json)
     : id = json['mal_id'] as int,
       title = json['title'] as String,
-      images = json['images'] as Map<String, dynamic>;
+      _images = json['images'] as Map<String, dynamic>,
+      _genres = json['genres'] as List<dynamic>;
 
-  String get thumbnail => images['jpg']['image_url'] as String;
+  String get thumbnail => _images['jpg']['image_url'] as String;
+
+  List<Genre> get genres => _genres
+      .map((json) => Genre.fromJson(json as Map<String, dynamic>))
+      .toList();
 }
--- lib/screens/browse_screen.dart
+++ lib/screens/browse_screen.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:mangatrack/models/genre.dart';
 import 'package:mangatrack/models/manga.dart';
+import 'package:mangatrack/widgets/manga_card.dart';
 
 import '../services/jikan_service.dart';
 
@@ -17,7 +18,7 @@ class _BrowseScreenState extends State<BrowseScreen>
     with AutomaticKeepAliveClientMixin {
   var genres = <Genre>[];
   var mangaList = <Manga>[];
-  Map<String, List<dynamic>> groupedByGenre = {};
+  Map<Genre, List<Manga>> groupedByGenre = {};
   bool isLoading = true;
 
   @override
@@ -48,9 +49,16 @@ class _BrowseScreenState extends State<BrowseScreen>
           debugPrint('[Browse] Manga loaded: ${mangaList.length}');
 
           // TODO: filter genres to only those with manga, and fetch more pages if needed to get a good sample of manga for each genre
-
-          // TODO: Group by  genre
-          groupedByGenre = {};
+          for (final genre in genres) {
+            // TODO: Group by  genre
+            for (final manga in mangaList) {
+              groupedByGenre.update(
+                genre,
+                (value) => [...value, manga],
+                ifAbsent: () => [manga],
+              );
+            }
+          }
 
           setState(() => isLoading = false);
         }
@@ -72,10 +80,69 @@ class _BrowseScreenState extends State<BrowseScreen>
 
     return Scaffold(
       appBar: AppBar(title: const Text('Browse')),
-      body: const Center(
-        // TODO: build two-panel genre browser layout (can refer to ZUS coffee app for inspiration)
-        child: Text('Genre browser will appear here.'),
-      ),
+      // TODO: build two-panel genre browser layout (can refer to ZUS coffee app for inspiration)
+      body: isLoading
+          ? Center(child: CircularProgressIndicator())
+          : Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: groupedByGenre.length,
+                      itemBuilder: (context, index) {
+                        final genre = groupedByGenre.keys.elementAt(index);
+
+                        return Text(genre.name);
+                      },
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: ListView.builder(
+                      itemCount: groupedByGenre.length,
+                      itemBuilder: (context, index) {
+                        final genre = groupedByGenre.keys.elementAt(index);
+                        final mangaList = groupedByGenre.entries
+                            .elementAt(index)
+                            .value;
+
+                        return Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Container(
+                              color: Theme.of(context).primaryColorLight,
+                              padding: EdgeInsets.symmetric(
+                                horizontal: 10,
+                                vertical: 5,
+                              ),
+                              margin: EdgeInsets.only(bottom: 10),
+                              child: Text('${genre.id}. ${genre.name}'),
+                            ),
+                            GridView.builder(
+                              shrinkWrap: true,
+                              physics: NeverScrollableScrollPhysics(),
+                              gridDelegate:
+                                  const SliverGridDelegateWithFixedCrossAxisCount(
+                                    crossAxisCount: 2,
+                                    crossAxisSpacing: 10,
+                                    mainAxisSpacing: 10,
+                                    childAspectRatio: 1 / 2,
+                                  ),
+                              itemCount: mangaList.length,
+                              itemBuilder: (context, index) =>
+                                  MangaCard(mangaList[index]),
+                            ),
+                          ],
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
     );
   }
 }
